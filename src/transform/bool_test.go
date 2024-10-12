ff16a4fd83117127c2f09e0bfa416ec26bf57884
olaf michaelis <o.mic@web.de>
2024-10-12 19:40:59 +0200

    Fix bool tests

--- src/transform/bool_test.go
+++ src/transform/bool_test.go
@@ -1,19 +1,29 @@
 package transform
 
 import (
+	"fmt"
+	"sam/src/conf"
 	"testing"
 )
 
 func TestBool(t *testing.T) {
-	// assert(tr.bool("true"), "true", t)
-	// assert(tr.bool("false"), "false", t)
-	// assert(tr.bool("enable"), "true", t)
-	// assert(tr.bool("disable"), "false", t)
-	// assert(tr.bool("enabled"), "true", t)
-	// assert(tr.bool("disabled"), "false", t)
-	// assert(tr.bool("on"), "true", t)
-	// assert(tr.bool("off"), "false", t)
-	// assert(tr.bool("1"), "true", t)
-	// assert(tr.bool("0"), "false", t)
-	// assert(tr.bool("any_other_string"), "false", t)
+	assertBool("true", true, t)
+	assertBool("false", false, t)
+	assertBool("enable", true, t)
+	assertBool("disable", false, t)
+	assertBool("enabled", true, t)
+	assertBool("disabled", false, t)
+	assertBool("on", true, t)
+	assertBool("off", false, t)
+	assertBool("1", true, t)
+	assertBool("0", false, t)
+	assertBool("any_other_string", false, t)
+}
+
+func assertBool(str string, exp bool, t *testing.T) {
+	conf := conf.New()
+	conf.String = str
+	tr = Init(conf)
+	res := fmt.Sprintf("%v", tr.bool())
+	assert(res, fmt.Sprintf("%v", exp), t)
 }
