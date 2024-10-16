356a4bd9a703a62f5cf1c1d55958e42d0c1c1787
olaf michaelis <o.mic@web.de>
2024-10-16 17:39:25 +0200

    Add path test

--- src/transform/path_test.go
+++ src/transform/path_test.go
@@ -1,17 +1,21 @@
 package transform
 
-// func TestDirName(t *testing.T) {
-// 	assert(tr.DirName("/home/username/document.txt"), "/home/username", t)
-// 	assert(tr.DirName("/home/username"), "/home", t)
-// 	assert(tr.DirName("/home/username/"), "/home", t)
-// 	assert(tr.DirName("/home/username///"), "/home", t)
-// }
+import (
+	"sam/src/conf"
+	"testing"
+)
 
-// func TestTidyFilePath(t *testing.T) {
-// 	assert(tr.TidyFilePath("/tmp/HEllO   WORLD___.md"), "/tmp/hello_world_.md", t)
-// 	assert(tr.TidyFilePath("/tmp/A B C D E.docx"), "/tmp/a_b_c_d_e.docx", t)
-// 	assert(tr.TidyFilePath("/tmp/1!2@3#4$5.docx"), "/tmp/1_2_3_4_5.docx", t)
-// 	assert(tr.TidyFilePath("/tmp/1!!!2!!!.docx"), "/tmp/1_2_.docx", t)
-// 	assert(tr.TidyFilePath("/tmp/hello-world!!!.txt"), "/tmp/hello-world_.txt", t)
-// 	assert(tr.TidyFilePath("/tmp/ä-_-Ö_-_ü.md"), "/tmp/ae_oe_ue.md", t)
-// }
+func TestPath(t *testing.T) {
+	assertPath("/hello//there/world", "dir", "/hello/there", t)
+	assertPath("/hello//there/world", "bn", "world", t)
+	assertPath("/hello//there/world.zip", "ext", ".zip", t)
+	assertPath("/hello//there/world.global", "ext", ".global", t)
+}
+
+func assertPath(str, target string, exp string, t *testing.T) {
+	conf := conf.New()
+	conf.String = str
+	conf.Target = target
+	tr := Init(conf)
+	assert(tr.runPath(), exp, t)
+}
