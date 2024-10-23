57f2b43be1e4372975601faf4ffb1bed4d083638
Olaf Michaelis <omichaelis@aip.de>
2024-10-23 09:03:15 +0200

    Add layout to date print

--- src/transform/date.go
+++ src/transform/date.go
@@ -110,11 +110,11 @@ func (tr Transform) now() time.Time {
 // }
 
 func (tr Transform) assembleDateTableContent(tim time.Time) (r [][]interface{}) {
-	r = append(r, []interface{}{"Format", "Date"})
+	r = append(r, []interface{}{"Format", "Date", "Layout"})
 	r = append(r, []interface{}{"Unix Time Stamp", tim.Unix()})
 	for _, el := range tr.DateLayouts {
 		if el.Print {
-			r = append(r, []interface{}{el.Name, tim.Format(el.Layout)})
+			r = append(r, []interface{}{el.Name, tim.Format(el.Layout), el.Layout})
 		}
 	}
 	return
